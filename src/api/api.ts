c411123db9fcb82160cb89e0f69050cceadf55e5
Tiroras <shnyrov2000@gmail.com>
2021-09-04 17:52:01 +0400

    debug

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,19 +2,19 @@
 
 const API = {
   getOperators(){
-    return fetch("http://localhost/api/operators").then((res: Response) => {
+    return fetch("https://brave-dev.vercel.app/api/operators").then((res: Response) => {
       return res.json();
     })
   },
   postOperator(id: number){
-    return fetch("http://localhost/api/operators", {
+    return fetch("https://brave-dev.vercel.app/api/operators", {
       method: "POST",
       body: JSON.stringify(id),
       headers: {'Content-Type': 'application/json'}
     }).then((res: Response) => res.json());
   },
   postPayment(operator_id: number, phoneNumber: string, sum: number){
-    return fetch("http://localhost/api/payment", {
+    return fetch("https://brave-dev.vercel.app/api/payment", {
       method: "POST",
       body: JSON.stringify({operator_id, phoneNumber, sum}),
       headers: {'Content-Type': 'application/json'}
